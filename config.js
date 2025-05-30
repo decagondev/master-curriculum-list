8fda94496579e307f72ee99fe51fdf574cb44d13
Jonathan Matthew Brunt <jon.m.brunt@gmail.com>
2025-05-30 04:10:53 -0300

    DS SPRINT 16 GCA LINK HOT FIX
    - in ds-unit-4-sprint-16:
    - in sprint-challenge:
    - update code for dynamic GCA link based on config.js in root
    
    - in config.js:
    - update GCA link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    GCA_TEST_URL: 'https://app.codesignal.com/public-test/m57r2MoaMLot7rL5M/fDc6W6at4G9bti'
+    GCA_TEST_URL: 'https://app.codesignal.com/standardized-test/GgvonNCksGzjey46c'
 };
 
 window.CONFIG = CONFIG;
